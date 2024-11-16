a11c2c278db310b4c1b9a937bfcef238325d07d5
ang-elote <angela.22chavez@gmail.com>
2024-11-15 20:24:34 -0600

    crear pags y linkearlas en el app js

--- echo/src/App.js
+++ echo/src/App.js
@@ -1,13 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SplashScreen from './pages/Splash';
+import Access from './pages/Access';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ChangePassword from './pages/Changepassword';
+import Home from './pages/Home';
+import MakePost from './pages/Makepost';
+import EditPost from './pages/Editpost';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Splash />} />
+      <Route path="/access" element={<Access />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/change-password" element={<Changepassword />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/make-post" element={<Makepost />} />
+      <Route path="/edit-post/:id" element={<Editpost />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
 
